2bf7e8a2d34bf29a1a360b96735696fceebb6528
Yonatan Ben Ami <yba6081@gmail.com>
2025-06-09 14:55:36 +0300

    add: sql

--- Malshinon/Program.cs
+++ Malshinon/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace IntelReport
+{
+    public class Malshinon
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
